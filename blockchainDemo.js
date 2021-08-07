e67715a326eff4dda74e5c0b6c0e8f5f24256ea0
MGreco2112 <mgreco2112@tutanota.com>
2021-08-07 17:36:55 -0400

    added proof of work method

--- blockchainDemo.js
+++ blockchainDemo.js
@@ -7,16 +7,28 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
+
 
 class Blockchain{
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
 
     }
 
@@ -30,7 +42,8 @@ class Blockchain{
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        // newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -53,17 +66,12 @@ class Blockchain{
 }
 
 let galtCoin = new Blockchain();
+
+console.log("Mining block 1...");
 galtCoin.addBlock(new Block(1, "01/02/2112", {amount: 4}));
+console.log("Mining block 2...");
 galtCoin.addBlock(new Block(2, "01/05/2112", {amount: 10}));
+console.log("Mining block 3...");
 galtCoin.addBlock(new Block(3, "01/09/2112", {amount: 50}));
 
-console.log(JSON.stringify(galtCoin, null, 4));
-
-console.log("Is blockchain valid? " + galtCoin.isChainValid());
-
-galtCoin.chain[1].data = {amount: 100};
-galtCoin.chain[1].hash = galtCoin.chain[1].calculateHash();
-
-console.log(JSON.stringify(galtCoin, null, 4));
-
-console.log("Is blockchain valid? " + galtCoin.isChainValid());
+// console.log(JSON.stringify(galtCoin, null, 4));
